a29fd5c132ae26aa7604d1da77ef1acecc6ef47b
David Perez <davperez89@gmail.com>
2017-12-15 01:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function theBeatlesPlay(musicians,instruments){
 function johnLennonFacts(facts){
   var array =[];
   let i=0
-  while (i < facts.length) 
+  while (i < facts.length){
+    array.push()
+  } 
 }
