c24c6b59ecd6b2a4df28d0fde2d292a4fa2617b2
David Perez <davperez89@gmail.com>
2017-12-15 01:27:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function theBeatlesPlay(musicians,instruments){
   var array = [];
-  for (var i=0; i < players.length; i++) {
+  for (let i=0; i < players.length; i++) {
       allPlayers.push(players[i] + "plays" + instruments[i]);
   }
   return allPlayers;
 }
 
++function theBeatlesPlay(musicians, instruments) {
+ +  var array = [];
+ +  for (let i = 0; i < musicians.length; i++) 
+ +  {
+ +    var string = `${musicians[i]} plays ${instruments[i]}`;
+ +    array.push(string);
+ +  }
+ +  return array
