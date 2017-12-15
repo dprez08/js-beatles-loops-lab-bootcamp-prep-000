bcd9ea48e34b3f569e5b475c9896a7f2f1357086
David Perez <davperez89@gmail.com>
2017-12-15 01:07:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-function theBeatlesPlay(musicians,instruments){
-  var all
-  
+function theBeatlesPlay(players,instruments){
+  var allPlayers = [];
+  for (var i=0; i < players.length; i++){
+      allPlayers.push(players[i] + "plays" + instruments[i]);
+  }
+  return allPlayers;
 }
 
-+
- +function theBeatlesPlay(players, instruments) {
- +    var allPlayers = [];
- +    for (var i=0; i < players.length; i++) {
- +        allPlayers.push(players[i] + " plays " + instruments[i]);
- +    }
- +    return allPlayers;
- +}
- +
