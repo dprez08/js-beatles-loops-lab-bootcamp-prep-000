093d15f9e45b37104369bece92fba490bef4bc2a
David Perez <davperez89@gmail.com>
2017-12-15 01:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function johnLennonFacts(facts){
   var array =[];
   let i=0
   while (i < facts.length){
-    array.push({facts[i]})
+    array.push(`${facts[i]}!!!`);
+    i++
   } 
+  return array
 }
+
